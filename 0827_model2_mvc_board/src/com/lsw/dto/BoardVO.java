1c5c78958e062edf9a1e6adc211fb3603e7442b5
ev15963 <ev15963@hs.ac.kr>
2020-08-27 11:25:44 +0900

    BoardVO 생성 11:25

--- 0827_model2_mvc_board/src/com/lsw/dto/BoardVO.java
+++ 0827_model2_mvc_board/src/com/lsw/dto/BoardVO.java
@@ -1,5 +1,98 @@
 package com.lsw.dto;
 
-public class BoardVO {
+import java.sql.Timestamp;
 
+public class BoardVO {
+	private Integer num;
+	private String name;
+	private String email;
+	private String pass;
+	private String title;
+	private String content;
+	private Integer readcount;
+	private Timestamp writedate;
+	
+	private BoardVO() {
+		
+	}
+	@Override
+	public String toString() {
+		String str = "==>";
+		str += (this.num+"__");
+		str += (this.name+"__");
+		str += (this.email+"__");
+		str += (this.pass+"__");
+		str += (this.title+"__");
+		str += (this.content+"__");
+		str += (this.readcount+"__");
+		str += (this.writedate+"__");
+		
+		return str;
+	}
+	
+	public Integer getNum() {
+		return num;
+	}
+
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getReadcount() {
+		return readcount;
+	}
+
+	public void setReadcount(Integer readcount) {
+		this.readcount = readcount;
+	}
+
+	public Timestamp getWritedate() {
+		return writedate;
+	}
+
+	public void setWritedate(Timestamp writedate) {
+		this.writedate = writedate;
+	}
+	
+	
 }
--- 0827_model2_mvc_board/src/com/lsw/util/DBManger.java
+++ 0827_model2_mvc_board/src/com/lsw/util/DBManger.java
@@ -1,5 +1,47 @@
 package com.lsw.util;
 
-public class DBManger {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DBManger {
+	private static String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static String id = "lsw";
+	private static String pwd = "1234";
+	
+	public void Connection() {
+		try {
+			Class.forName(url);
+			Connection conn = DriverManager.getConnection(url, id, pwd);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage()+"");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("close csr err : "+e.getMessage());
+		}
+		
+	}
+	
+	public static void close(Connection conn, Statement stmt) {
+		try {
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("close cs err : "+e.getMessage());
+		}
+		
+	}
 }
