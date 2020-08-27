1b89b3841aff3916a302040c2c5d11e26917d6bb
ev15963 <ev15963@hs.ac.kr>
2020-08-27 11:39:33 +0900

    BoardDAO 메서드 명만 생성 11:39

--- 0827_model2_mvc_board/src/com/lsw/dao/BoardDAO.java
+++ 0827_model2_mvc_board/src/com/lsw/dao/BoardDAO.java
@@ -1,5 +1,49 @@
 package com.lsw.dao;
 
+import java.util.List;
+
+import com.lsw.dto.BoardVO;
+
 public class BoardDAO {
+	private static BoardDAO instance = new BoardDAO();
+
+	private BoardDAO() {
+
+	}
+
+	public static BoardDAO getInstance() {
+		return instance;
+
+	}
+
+	public List<BoardVO> selectAllBoards() {
+		return null;
+
+	}
+
+	public void insertBoard(BoardVO bVo) {
+
+	}
+
+	public void updateReadCount(String num) {
+
+	}
+
+	// 게시판 글 상세 내용 보기 : 글번호로 찾아온다 : 실패 null
+	public BoardVO selectOneBoardByNum(String num) {
+
+	}
+
+	public void updateBoard(BoardVO bVo) {
+
+	}
+
+	public BoardVO checkPassWord(String pass, String num) {
+
+	}
+
+	public void deleteBoard(String num) {
+
+	}
 
 }
