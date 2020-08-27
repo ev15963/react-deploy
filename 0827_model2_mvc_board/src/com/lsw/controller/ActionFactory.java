bae814a6b847ad560e6e0dc90063bc3225d7be0b
ev15963 <ev15963@hs.ac.kr>
2020-08-27 10:40:28 +0900

    action getInstance, getAction 추가 10:40

--- 0827_model2_mvc_board/src/com/lsw/controller/ActionFactory.java
+++ 0827_model2_mvc_board/src/com/lsw/controller/ActionFactory.java
@@ -1,5 +1,17 @@
 package com.lsw.controller;
 
+import com.lsw.controller.action.Action;
+
 public class ActionFactory {
+
+	public static ActionFactory getInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Action getAction(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
