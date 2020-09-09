8a73576ba8a73893ff658fbf144b00b2a5cb6929
ev15963 <ev15963@hs.ac.kr>
2020-09-09 12:30:44 +0900

    ajax.html 설정 12:30

--- 0909_jquery_ajax/src/com/lsw/dto/Member.java
+++ 0909_jquery_ajax/src/com/lsw/dto/Member.java
@@ -2,4 +2,40 @@ package com.lsw.dto;
 
 public class Member {
 
+	private String name;
+	private String age;
+	private String address;
+	
+	public Member() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Member [name=" + name + "age=" + age + ", address=" + address + "]";
+	}
 }
