7973f2e0360cc0b31813cdeae81f6ad933c19e28
ev15963 <ev15963@hs.ac.kr>
2020-09-10 16:31:52 +0900

    bookApp.html 수정, dbhandldle 생성 16:31

--- 0910_jquery_xml_bookApp/WebContent/dbHandle.js
+++ 0910_jquery_xml_bookApp/WebContent/dbHandle.js
@@ -1,49 +1,69 @@
 var db = null;
-var var_no = null;
+var var_no = null; // 페이지 받을 변수
 var position = null;
 var index;
- 
-// 데이터베이스 생성 및 오픈
-function openDB(){
 
-} 
+// 데이터베이스 생성 및 오픈
+function openDB() {
+	db = window.openDatabase('bookDB', '1.0', '북DB', 1024 * 1024);
+	console.log('1_DB 생성...');
+}
 
 // 테이블 생성 트랜잭션 실행
 function createTable() {
+	db
+			.transaction(function(tr) {
+				var createSQL = 'create table if not exists book(type text, name text)';
 
- } 
+				tr.executeSql(createSQL, [], function() {
+					console.log('2_1_테이블 생성_sql 실행성공...');
+				}, function() {
+					console.log('2_1_테이블 생성_sql 실행 실패...');
+				});
+			},
+			
+			function() {
+				console.log('테이블 생성 트랜잭션 실패... 롤백은 자동');
+			},
+			
+			function() {
+				console.log('테이블 생성 트랜잭션 성공...');
+			}
+			
+		);
+}
 
- // 데이터 입력 트랜잭션 실행
- function insertBook(){ 
-    
- }
+// 데이터 입력 트랜잭션 실행
+function insertBook() {
+
+}
 
 // 전체 데이터 검색 트랜잭션 실행
-function listBook(){
-        
+function listBook() {
+
 }
 
 // 데이터 수정 트랜잭션 실행
-function updateBook(){
-    
+function updateBook() {
+
 }
 
 // 데이터 삭제 트랜잭션 실행
-function deleteBook(){
-      
-} 
+function deleteBook() {
+
+}
 
 // 데이터 수정 위한 데이터 검색 트랜잭션 실행
-function selectBook2(name){
-      
+function selectBook2(name) {
+
 }
 
 // 데이터 삭제 위한 데이터 검색 트랜잭션 실행
-function selectBook3(name){
-        
+function selectBook3(name) {
+
 }
 
 // 데이터 조건 검색 트랜잭션 실행
-function selectBook4(name){
- 
- };
+function selectBook4(name) {
+
+};
