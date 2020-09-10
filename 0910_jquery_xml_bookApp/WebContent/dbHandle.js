0b749b7ed3f47787c43e0f08437d4f4a893f2b5b
ev15963 <ev15963@hs.ac.kr>
2020-09-10 16:23:41 +0900

    bookapp.html dbhandle.js 생성 16:00

--- 0910_jquery_xml_bookApp/WebContent/dbHandle.js
+++ 0910_jquery_xml_bookApp/WebContent/dbHandle.js
@@ -1,3 +1,49 @@
-/**
- * 
- */
+var db = null;
+var var_no = null;
+var position = null;
+var index;
+ 
+// 데이터베이스 생성 및 오픈
+function openDB(){
+
+} 
+
+// 테이블 생성 트랜잭션 실행
+function createTable() {
+
+ } 
+
+ // 데이터 입력 트랜잭션 실행
+ function insertBook(){ 
+    
+ }
+
+// 전체 데이터 검색 트랜잭션 실행
+function listBook(){
+        
+}
+
+// 데이터 수정 트랜잭션 실행
+function updateBook(){
+    
+}
+
+// 데이터 삭제 트랜잭션 실행
+function deleteBook(){
+      
+} 
+
+// 데이터 수정 위한 데이터 검색 트랜잭션 실행
+function selectBook2(name){
+      
+}
+
+// 데이터 삭제 위한 데이터 검색 트랜잭션 실행
+function selectBook3(name){
+        
+}
+
+// 데이터 조건 검색 트랜잭션 실행
+function selectBook4(name){
+ 
+ };
