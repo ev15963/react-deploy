7e628a175ae173e41e22be3ab62a6a98c3aef960
ev15963 <ev15963@hs.ac.kr>
2020-10-05 16:54:11 +0900

    actionfactory 대응 16:54

--- 0928_specialproject/src/BoardServlet.java
+++ 0928_specialproject/src/BoardServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.lsw.controller.ActionFactory;
+import com.lsw.controller.action.Action;
+
 /**
  * Servlet implementation class BoardServlet
  */
@@ -31,7 +34,22 @@ public class BoardServlet extends HttpServlet {
 		
 		System.out.println("BoardServlet 에서 요청을 받음을 확인"+command);
 		//command값이 무엇인지 콘솔창에서 확인
+		
+		ActionFactory af = ActionFactory.getInstance();
+		//ActionFactory의 getInstance에는 return instance가 들어있다
+		//instance는 ActionFactory 객체화한 싱글톤
+		
+		Action action = af.getAction(command);
+		//ActionFactory 클래스에서 불러온
+		//Action 클래스의 getAction의 (String command)를 인자로 받던 것을 Action action에 담는다.
+		
+		if(action != null) { //만일 action이 null이 아닐때 (action에 command 값이 들어와 있을때)
+			action.execute(request, response); //action의 request와 response를 담아서 실행한다.
+			//execute - SQL문이 무엇인지 모를때사용
+			//반환은 true 이면 Resultset객체에, false이면 결과가 없거나 갱신된 행의 수의 경우
+		}
 	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
